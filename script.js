108c8861c596981a8896c99a06d0f77af18efb00
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-02-15 00:35:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+const saveButton = document.getElementById("save");
+const resetButton = document.getElementById("reset");
+const cgpaField = document.getElementById("cgpa");
+const phoneField = document.getElementById("phone");
+const yearField = document.getElementById("year");
+const durationField = document.getElementById("duration");
+
+resetButton.addEventListener("click", () => {
+  const forms = document.getElementsByTagName("form");
+  for (let i = 0; i < forms.length; i++) {
+    forms[i].reset();
+  }
+});
+
+saveButton.addEventListener("click", () => {
+  let valid = true;
+
+  const forms = document.getElementsByTagName("form");
+  for (let i = 0; i < forms.length; i++) {
+    const form = forms[i];
+    const inputs = form.getElementsByTagName("input");
+    for (let j = 0; j < inputs.length; j++) {
+      const input = inputs[j];
+      if (input.value === "") {
+        valid = false;
+      }
+    }
+  }
+
+  if (valid) {
+    alert("Resume saved!");
+    window.location.href = "data-saved.html"; // Redirect to a new webpage
+    for (let i = 0; i < forms.length; i++) {
+      forms[i].reset(); // Reset all the fields
+    }
+  } else {
+    alert("All fields must be filled!");
+  }
+});
+
+cgpaField.addEventListener("change", function () {
+  const cgpaValue = cgpaField.value;
+
+  if (cgpaValue > 4) {
+    alert("CGPA can not be greater than 4!");
+    cgpaField.value = "";
+  } else if (cgpaValue < 1) {
+    alert("CGPA can not be less than 1!");
+    cgpaField.value = "";
+  } else {
+  }
+});
+
+phoneField.addEventListener("change", function () {
+  const phoneValue = phoneField.value;
+  if (!/^\+(?:[0-9()\s-] ?){6,14}[0-9]$/.test(phoneValue)) {
+    alert(
+      "Phone number can only contain numbers, spaces, hyphens, and parentheses!"
+    );
+    phoneField.value = "";
+  }
+});
+
+yearField.addEventListener("change", function () {
+  const yearValue = yearField.value;
+
+  if (!/^\d*$/.test(yearValue)) {
+    alert("Year can only contain numbers!");
+    yearField.value = "";
+  }
+});
+
+durationField.addEventListener("change", function () {
+  const durationValue = durationField.value;
+
+  if (!/^\d*$/.test(durationValue)) {
+    alert("Duration can only contain numbers!");
+    durationField.value = "";
+  }
+});
